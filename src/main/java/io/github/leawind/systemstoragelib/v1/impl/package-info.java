e15145d8bb94b14dc0ca87a763beacaeb2c51227
Leawind <leawind@yeah.net>
2026-05-28 07:59:31 +0800

    refactor: add @ApiStatus.Internal to package impl and utils

---
+++ src/main/java/io/github/leawind/systemstoragelib/v1/impl/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package io.github.leawind.systemstoragelib.v1.impl;
+
+import org.jetbrains.annotations.ApiStatus;
---
+++ src/main/java/io/github/leawind/systemstoragelib/v1/utils/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package io.github.leawind.systemstoragelib.v1.utils;
+
+import org.jetbrains.annotations.ApiStatus;
